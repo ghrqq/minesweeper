03ac95b7c85a7a0d2cde2b02489498847a5a9345
Theo <theoozdev@gmail.com>
2025-03-31 19:36:30 +0200

    modules exposed for mf

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require("@vue/cli-service");
+const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "vuePlayground",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./TheosMineSweeper": "./src/views/MineSweeperView.vue",
+          "./TheosChatBot": "./src/views/ChatBotView.vue",
+        },
+        shared: {
+          vue: { singleton: true, eager: true, requiredVersion: "^3.2.13" },
+          "vue-router": { singleton: true, eager: true },
+        },
+      }),
+    ],
+  },
 });
